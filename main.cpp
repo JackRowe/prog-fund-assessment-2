b89ae8ebaad55152cfc552f0b92588edd4794229
Jack Rowe <jack.rowe@outlook.com>
2023-10-24 00:32:24 +0100

    output txt files to console & qol functions for output

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    outputFile("grid.txt", { 0,0 });
+    outputFile("menu.txt", { 14, 0 });
+    //outputFile("x.txt", outputPositions[1]);
+    //outputFile("o.txt", outputPositions[8]);
+    //setOuputCursorPosition({ 10,11 });
 }
 
 // TIC TAC TOE
@@ -25,15 +29,15 @@ ____|____|____
     |    |
 
     |    |
-    |    |
-    |    |
-____|____|____
-    |    |
-    |    |
+ 1  | 2  | 3
     |    |
 ____|____|____
     |    |
+ 4  | 5  | 6
     |    |
+____|____|____
+    |    |
+ 7  | 8  | 9
     |    |
     |    |
 
--- main.h
+++ main.h
@@ -1,5 +1,11 @@
 #pragma once
 
 #include <iostream>
-#include <fstream>
-#include "grid.txt";
+#include <map>
+#include "console.h"
+
+std::map<int, COORD> outputPositions{
+	{1, {1,1}}, {2, {6,1}}, {3, {11,1}},
+	{4, {1,5}}, {5, {6,5}}, {6, {11,5}},
+	{7, {1,9}}, {8, {6,9}}, {9, {11,9}},
+};
