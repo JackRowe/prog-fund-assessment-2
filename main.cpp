c41d6efc85e5250f5d4593b74e9466995ef95194
Jack Rowe <jack.rowe@outlook.com>
2023-10-26 10:49:30 +0100

    COMJIT

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 // temp include
 #include <iostream>
 
+
+
 int main()
 {
 
-	for (int i = 0; i < 10000; i++) {
-		Sleep(1);
+	for (int i = 0; i < 100000; i++) {
+		Sleep(0.1);
 		POINT consolePosition = getConsolePosition();
 		POINT mousePosition = getMousePosition();
 		COORD consoleSize = getConsoleSize();
-		COORD differenceCoord = consolePosition - mousePosition;
+		//COORD differenceCoord = consolePosition - mousePosition;
 		int difference = sqrt(pow(mousePosition.x - consolePosition.x, 2) + pow(mousePosition.y - consolePosition.y, 2));
 
 		std::cout << difference << std::endl;
-		//setConsolePosition({ short((mousePosition.x + consoleSize.X) + 33 ), short(mousePosition.y - 15) });
+		setConsolePosition({ short((mousePosition.x + consoleSize.X) + 30 ), short(mousePosition.y - 15) });
 	}
 
 }
