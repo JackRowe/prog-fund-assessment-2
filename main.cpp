d02416ae11155630a253e5dc9ae1ff343e899a55
Jack Rowe <jack.rowe@outlook.com>
2023-10-26 10:09:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,10 +11,11 @@ int main()
 		POINT consolePosition = getConsolePosition();
 		POINT mousePosition = getMousePosition();
 		COORD consoleSize = getConsoleSize();
+		COORD differenceCoord = consolePosition - mousePosition;
 		int difference = sqrt(pow(mousePosition.x - consolePosition.x, 2) + pow(mousePosition.y - consolePosition.y, 2));
 
 		std::cout << difference << std::endl;
-		setConsolePosition({ short((mousePosition.x + consoleSize.X) + 33 ), short(mousePosition.y - 15) });
+		//setConsolePosition({ short((mousePosition.x + consoleSize.X) + 33 ), short(mousePosition.y - 15) });
 	}
 
 }
